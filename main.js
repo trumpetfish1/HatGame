cc6817046f34cee113982908979087fc91cfe290
trumpetfish1 <trumpetfish1@gmail.com>
2025-07-24 16:52:35 -0400

    Create game map

--- main.js
+++ main.js
@@ -4,3 +4,32 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+const mapDict = [hole, fieldCharacter]
+
+
+class Field {
+    _fieldArray = []
+
+    generate(){
+        let field = [];
+        for (let i = 0; i < 4; i++) {
+            let row = [];
+            for (let j = 0; j < 4; j++) {
+             row.push(mapDict[Math.floor(Math.random() * mapDict.length)]);
+                  }
+        field.push(row);}
+        this._fieldArray = field;
+ 
+    }
+
+   print() {
+    console.log(this._fieldArray);
+    for (let row of this._fieldArray) {
+        console.log(row.join(''))
+    }
+}
+}
+
+field = new Field();
+field.generate()
+field.print()
