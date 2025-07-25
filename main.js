578a07966c736a2c34482dc1aa55b03e2d84556b
trumpetfish1 <trumpetfish1@gmail.com>
2025-07-24 23:04:44 -0400

    Develop core game logic

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ 
 const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
@@ -7,29 +8,97 @@ const pathCharacter = '*';
 const mapDict = [hole, fieldCharacter]
 
 
+
 class Field {
     _fieldArray = []
+    _goalX = 0
+    _goalY = 0
+    _startX = 0
+    _startY = 0
+    _playerX = 0
+    _playerY = 0
 
-    generate(){
+    generate(n){
         let field = [];
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < n; i++) {
             let row = [];
-            for (let j = 0; j < 4; j++) {
+            for (let j = 0; j < n; j++) {
              row.push(mapDict[Math.floor(Math.random() * mapDict.length)]);
-                  }
+        }
         field.push(row);}
         this._fieldArray = field;
- 
+        this._goalX = Math.floor(Math.random() * this._fieldArray.length)
+        this._goalY = Math.floor(Math.random() * this._fieldArray.length)
+        this._startX = Math.floor(Math.random() * this._fieldArray.length)
+        this._startY = Math.floor(Math.random() * this._fieldArray.length)
+}
+
+    print() {
+        for (let row of this._fieldArray) {
+            console.log(row.join(' '))
+        }
     }
 
-   print() {
-    console.log(this._fieldArray);
-    for (let row of this._fieldArray) {
-        console.log(row.join(''))
+    initializeObjects() {
+    //  console.log(this._fieldArray.length)
+        this._fieldArray[this._goalX][this._goalY] = '^'
+        this._fieldArray[this._startX][this._startY] = '*'
+        }
+
+    navigateField() {
+        let playing = true;
+        this._playerX = this._startX
+        this._playerY = this._startY
+        
+    while (playing) {
+        console.clear()
+        this.print();
+        const move = prompt('Which direction? (w/a/s/d): ').toLowerCase();
+
+      // Erase current position
+        ;
+
+        switch (move) {
+        case 'w':
+            this._playerX--;
+
+            break;
+        case 's':
+            this._playerX++;
+            break;
+        case 'a':
+            this._playerY--;
+            break;
+        case 'd':
+            this._playerY++;
+            break;
+        default:
+            console.log('Invalid input. Use w/a/s/d.');
+            continue
+            }
+            if (this._fieldArray[this._playerX][this._playerY] === 'O') {
+                console.log('You fell in a hole! You dead');
+                this._fieldArray[this._playerX][this._playerY] = 'X'
+                playing = false
+            }
+            if (this._fieldArray[this._playerX][this._playerY] === '^') {
+                console.log('Son of a gun, you found ye olde faithful! sombrero');
+            }
+            else {
+                this._fieldArray[this._playerX][this._playerY] = '*'}
+        }
+
     }
-}
+
+
+    play() {
+        field.navigateField();
+
+    }
+
 }
 
 field = new Field();
-field.generate()
-field.print()
+field.generate(10);
+field.initializeObjects();
+field.play()
